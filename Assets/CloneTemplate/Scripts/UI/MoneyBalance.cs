964f2cd42a3955cea48e5de492c33696cc1e81bc
Alex Semchenkov <zigola12@list.ru>
2021-09-10 17:40:12 +0300

    Commit with bug to delete money after buying
    Fix this bug at home

--- Assets/CloneTemplate/Scripts/UI/MoneyBalance.cs
+++ Assets/CloneTemplate/Scripts/UI/MoneyBalance.cs
@@ -9,9 +9,19 @@ public class MoneyBalance : MonoBehaviour
     [SerializeField] private TMP_Text _money;
     [SerializeField] private Player _player;
 
-    private void Update()
+    private void OnEnable()
     {
         _money.text = _player.Money.ToString();
+        _player.MoneyChanged += OnMoneyChanged;
+    }
+    public void OnDisable()
+    {
+        _player.MoneyChanged -= OnMoneyChanged;
+    }
+
+    private void OnMoneyChanged(int money)
+    {
+        _money.text = money.ToString();
     }
 
 
